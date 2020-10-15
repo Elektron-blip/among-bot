21936add4f23b3854a7fd59e19c9ec6af1b001f4
Elektron-blip <somanshu.mendon@icloud.com>
2020-10-15 21:13:49 +0530

    Added a feature where it sends a message through a webhook when the bot comes online.

--- cogs/startup.py
+++ cogs/startup.py
@@ -1,14 +1,18 @@
 import discord
 from discord.ext import commands
+from main import logWebhook
 
 class Startup(commands.Cog):
 
-    def __init__(self,client):
+    def __init__(self,client,logWebhook):
         self.client=client
+        self.logWebhook=logWebhook
 
     @commands.Cog.listener()
     async def on_ready(self):
         print(f'Logged in as: {self.client.user}\nWith ID: {self.client.user.id}')
+        self.logWebhook.send(conetnt=f'I have come online!')
+
 
 def setup(client):
     client.add_cog(Startup(client))
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import discord
 import os
 from discord.ext import commands
+import requests
 
-client=commands.Bot(command_prefix=',')
+client = commands.Bot(command_prefix=',')
 
 for filename in os.listdir('./cogs'):
-    if filename.endswith=='.py':
+    if filename.endswith == '.py':
         client.load_extension(f'cogs.{filename[:-3]}')
+
+logWebhook = discord.Webhook.from_url(url=os.environ.get('LOG_WEBHOOK_URL'), adapter=discord.RequestsWebhookAdapter())
 
 client.run(os.environ.get('TOKEN'))
