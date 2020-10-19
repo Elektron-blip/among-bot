805bd574be2815ead9f6fb7ac9a28439e63c132e
Elektron-blip <somanshu.mendon@icloud.com>
2020-10-19 17:01:53 +0530

    Added a reset command to reset the room name after the game ends.

--- cogs/game.py
+++ cogs/game.py
@@ -18,6 +18,17 @@ class Game(commands.Cog):
                 await ctx.send('You need to be in a voice channel to use this command.')
         else:
             await ctx.send(f'You dont have the neccessary permissions to run this command. (You will need either MANAGE_GUILD permissions or the manager role to be able to use this command)')
+    
+    @commands.command(brief='Rests the name of the voice channel.')
+    async def reset(self,ctx):
+        if ctx.message.author.guild_permissions>= discord.Permissions(manage_guild=True) or commands.has_role(os.environ.get('MANAGER_ROLE_ID')):
+            if ctx.message.author.voice.channel is not None:
+                await ctx.message.author.voice.channel.edit(name='game-chat',reason=f'{ctx.message.author} reset the room name.')
+                await ctx.send(f'I have changed the name of your voice channel to `game-chat`')
+            else:
+                await ctx.send('You need to be in a voice channel to use this command.')
+        else:
+            await ctx.send(f'You dont have the neccessary permissions to run this command. (You will need either MANAGE_GUILD permissions or the manager role to be able to use this command)')
 
 def setup(client):
     client.add_cog(Game(client))
