90c8d32acfda3e18709628d01dd24341208c5bdc
Elektron-blip <somanshu.mendon@icloud.com>
2020-10-19 17:15:19 +0530

    I dunno whats up with the logging.

--- cogs/error.py
+++ cogs/error.py
@@ -6,11 +6,12 @@ class Error(commands.Cog):
 
     def __init__(self,client):
         self.client=client
+        self.logWebhook=logWebhook
 
     @commands.Cog.listener()
     async def on_error(self,error):
         print(error)
-        logWebhook.send(content=f'```console\n{error}\n```')
+        self.logWebhook.send(content=f'```console\n{error}\n```')
 
 def setup(client):
     client.add_cog(Error(client))
